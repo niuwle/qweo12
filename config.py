9280942e292a463b6be6582e9bded07a5165237a
niuwle <39056054+niuwle@users.noreply.github.com>
2023-07-28 17:46:47 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_key = 'MCZsl4KLqluPHodGQzSin0j3u8rrvAOXnDm8EkpaMPppQbYz0VZxZc08BDfHmlot'
-api_secret = 'JWqr3BMATJebbakC3WQjNsKhLHPItpMPPsmc81TU41EmDlI3o6YxmppD7FEOq3oa'
+api_key = 'asd'
+api_secret = 'asd'
 investment_percentage = 0.1  # Invest 10% of total portfolio in each coin
 newsapi = 'ba36febdc6fb4b98975406a0841c6eca'
